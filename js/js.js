95132274e641580f9f8bc9af600eae3d9dbe9727
TRI VI <trivi2710@gmail.com>
2017-05-02 10:19:54 -0400

    Adding a line break between author and button
    Chore: line break and jQuery Color
    
    Added line break between author line and button line. Included the
    jQuery Color script to change the color of elements when the new quote
    button is pressed

--- js/js.js
+++ js/js.js
@@ -1,24 +1,34 @@
-$(function(){
-	var currentQuote = "";
-	var currentAuthor = "";
+var currentQuote = "";
+var currentAuthor = "";
+var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
+function getQuote() {
+	$.ajax ({
+		headers: {
+			"X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
+			Accept: "application/json",
+  			"Content-Type": "application/x-www-form-urlencoded"
+  		},
+		url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
+		success: function(response) {
+			var r = JSON.parse(response);
+			currentQuote = r.quote;
+			currentAuthor = r.author;
+			$(".text").html(currentQuote);
+			$(".author").html(currentAuthor);
 
-	function getQuote() {
-		$.ajax ({
-			headers: {
-				"X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
-				Accept: "application/json",
-      			"Content-Type": "application/x-www-form-urlencoded"
-      		},
-			url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
-			success: function(response) {
-				var r = JSON.parse(response);
-				currentQuote = r.quote;
-				currentAuthor = r.author;
-				$(".text").html(currentQuote);
-				$(".author").html(currentAuthor);
-			}
-		})
-	}
+
+			// Change color of elements
+			var color = Math.floor(Math.random() * colors.length);
+			//console.log(color);
+			$(".container-fluid").animate({
+				backgroundColor: colors[color],
+				color: colors[color]
+			});
+		}
+	})
+}
+
+$(function(){
 	getQuote();
 	$("#new-quote").on('click', getQuote);
 });
